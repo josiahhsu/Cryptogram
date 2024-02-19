6536d116ddc1dacc703b056166669cac64dc38c4
josiahhsu <77859877+josiahhsu@users.noreply.github.com>
2024-02-18 18:04:48 -0600

    First shot at adding a cracker
    doesn't work very well at the moment - purely uses character frequencies, doesn't take anything else into account

--- cryptogram.js
+++ cryptogram.js
@@ -24,9 +24,23 @@ class Translator
     }
 }
 
+class Charfreq
+{
+    constructor(char, freq)
+    {
+        this.char = char;
+        this.freq = freq;
+    }
+}
+
 function is_upper(char)
 {
 	return alpha.includes(char);
+}
+
+function is_alpha(char)
+{
+    return is_upper(char.toUpperCase());
 }
 
 function translate(plaintext, key, encipher)
@@ -39,4 +53,50 @@ function translate(plaintext, key, encipher)
 		result += translator.translate(c, encipher);
 	}
 	return result;
+}
+
+function sort_charfreqs(charfreqs)
+{
+    charfreqs.sort((a,b)=>{ return b.freq - a.freq; });
+}
+
+function standard_charfreqs()
+{
+    const freqs =
+		[0.07984, 0.01511, 0.02504, 0.04260, 0.12452, 0.02262, 0.02013,
+		 0.06384, 0.0700, 0.00131, 0.00741, 0.03961, 0.02629, 0.06876,
+		 0.07691, 0.01741, 0.00107, 0.05912, 0.06333, 0.09058, 0.02844,
+		 0.01056, 0.02304, 0.00159, 0.02028, 0.00057];
+
+    let charfreqs = [];
+    for (var i = 0; i < freqs.length; i++)
+        charfreqs.push(new Charfreq(alpha[i], freqs[i]));
+
+    sort_charfreqs(charfreqs);
+
+    return charfreqs;
+}
+
+function find_key(ciphertext)
+{
+    let len = 0;
+    let charcounts = {};
+    for (const c of alpha)
+        charcounts[c] = 0;
+
+    for (const c of ciphertext)
+    {
+        if (is_alpha(c))
+        {
+            charcounts[c.toUpperCase()]++;
+            len++;
+        }
+    }
+
+    let charfreqs = [];
+    for (const c of alpha)
+        charfreqs.push(new Charfreq(c, charcounts[c]/len));
+
+    sort_charfreqs(charfreqs);
+    return charfreqs;
 }
